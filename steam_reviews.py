5f8b96ca5983bb4f7fac363d97a252ed0505314d
Nicola Mustone <mustone.nicola@gmail.com>
2025-11-09 11:00:55 +0200

    Update steam_reviews.py
    Correct license

--- steam_reviews.py
+++ steam_reviews.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
-# GPLv2 License
+# GPLv3 License
 # Copyright (c) 2025 Nicola Mustone
 # Author: Nicola Mustone (https://nicolamustone.blog)
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License version 2.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 import sys
 import argparse
